4696956d7534aad61c69163b22aa55983e81cf83
Seawhite <52661206+AnotherSeawhite@users.noreply.github.com>
2026-04-26 21:05:48 +0900

    Add link to wiki
    Replaces the tips with a link to wiki.

--- scripts/stats.js
+++ scripts/stats.js
@@ -3,18 +3,7 @@ import { IPC, PROTO } from './IPC/ipc';
 import { world } from '@minecraft/server';
 
 // https://github.com/azurite-bedrock/Sweep-N-Slash/blob/main/CROSS_COMPATIBILITY_GUIDE.md
-
-/* Possible flags:
-- is_weapon: Whether the item should be treated as a weapon. If not set, attacking with this item will deplete durability by 2.
-- sweep: Whether the item should have sweeping attack behavior.
-- disable_shield: Whether the item should disable shields when hit.
-- skip_lore: Whether to skip adding lore to the item.
-- no_inherit: Whether the item should not inherit the shooter's velocity when thrown/shot.
-- hide_indicator: Whether to hide the attack indicator when using this item.
-- kinetic_weapon: Whether the item has kinetic weapon component. If set, holding interact will disable custom damage behavior and use vanilla mechanics. This is *required* for kinetic weapons to work properly.
-- custom_cooldown: Whether the item uses vanilla item cooldown. If set, attack indicator will show the item cooldown instead.
-- mace: Whether the item has mace behavior. If set, falling for more than 1.5 blocks while holding the item will disable custom damage behavior and use vanilla mechanics. This is *not recommended* for custom items, as it is only a requirement for changing vanilla mace.
-*/
+// https://github.com/azurite-bedrock/Sweep-N-Slash/wiki/Formats
 
 const weaponStats = [
     {
